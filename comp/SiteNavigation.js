a080bfca250c4191fa407ec29f92483f8ecb6850
seohyun9672 <ajeong4@my.bcit.ca>
2023-04-01 11:05:28 -0700

    fix styles

--- comp/SiteNavigation.js
+++ comp/SiteNavigation.js
@@ -13,14 +13,18 @@ export default function SiteNavigation() {
   if (session) {
     return (
       <div className='navbar'>
-        <div className='navbar-btncont'>
-          <button className='navbar-button' onClick={signIn}>Sign Out</button>
-          <button className='navbar-button' onClick={() => router.push("/profile")}>Profile</button>
-          <div className='icon-button'>
-            <button
-              onClick={() => router.push("/posts")
-              }>Add your own recipe</button>
+        <div className='navbar-container'>
+          <img className='logo' src='/imgs/logo.svg' onClick={() => router.push("/")} />
+          <div className='navbar-btncont'>
+            <button className='navbar-button' onClick={signIn}>Sign Out</button>
+            <button className='navbar-button' onClick={() => router.push("/profile")}>Profile</button>
+            <div className='icon-button'>
             <PlusIcon width={20} />
+              <button
+                onClick={() => router.push("/posts")
+                }>Add your own recipe</button>
+              
+            </div>
           </div>
         </div>
       </div>
